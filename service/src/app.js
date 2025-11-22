d583140ad901264b3fbf73bc6d194f3f685e45ad
Suman Kayal <suman13kayal@gmail.com>
2025-11-22 22:28:19 +0530

    url update both backend and frontend

--- service/src/app.js
+++ service/src/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 // 👉 3) CORS AFTER json() and cookieParser()
 app.use(
     cors({
-        origin: process.env.FRONT_END_URL || "http://localhost:5173",
+        origin: process.env.FRONT_END_URL || "https://foodie-pu74.onrender.com",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         allowedHeaders: ["Content-Type", "Authorization"],
